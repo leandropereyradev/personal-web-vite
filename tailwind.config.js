9c88f4f81d718b9e8c35cec25b53ee64e0654fb7
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-17 12:45:30 +0100

    Install Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        fullHD: { raw: "(min-width: 1900px)" },
+      },
+      gridTemplateColumns: {
+        navbar: "30% 70%",
+        "layout-desktop": "40% 60%",
+      },
+      colors: {
+        "dark-grey": "#131313",
+      },
+    },
+  },
+  plugins: [],
+};
