f5a86423868f5ccc6b5f3517db8a38cb0f391248
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-18 10:19:20 +0100

    Fix desktop style Header component

--- src/components/sidebar/Header.jsx
+++ src/components/sidebar/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="flex flex-col gap-4 pb-6 md:pb-10 md:gap-10 lg:gap-12 xl:gap-8 fullHD:gap-12">
+    <header className="flex flex-col gap-4 md:gap-10 lg:gap-12 xl:gap-8 fullHD:gap-12">
       <h1 className="font-bold text-6xl leading-tight md:text-8xl lg:text-[8.5rem] xl:text-[5.5rem] fullHD:text-9xl">
         Leandro <br />
         <span className="block">Pereyra</span>
