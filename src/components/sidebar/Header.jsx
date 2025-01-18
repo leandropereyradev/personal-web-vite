7e9b6c246393730c207bbc6c56a7d6cd1b57853e
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-18 10:08:15 +0100

    Header desktop style

--- src/components/sidebar/Header.jsx
+++ src/components/sidebar/Header.jsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
-    <header className="flex flex-col gap-10 pb-6 md:pb-10 md:gap-14 lg:gap-20">
-      <h1 className="font-bold text-6xl leading-tight md:text-8xl lg:text-[8.5rem]">
+    <header className="flex flex-col gap-4 pb-6 md:pb-10 md:gap-10 lg:gap-12 xl:gap-8 fullHD:gap-12">
+      <h1 className="font-bold text-6xl leading-tight md:text-8xl lg:text-[8.5rem] xl:text-[5.5rem] fullHD:text-9xl">
         Leandro <br />
         <span className="block">Pereyra</span>
       </h1>
-      <h2 className="text-2xl md:text-4xl lg:text-5xl">Desarrollador Web</h2>
+      <h2 className="text-2xl md:text-4xl lg:text-6xl xl:text-[2.3rem] fullHD:text-[3.4rem]">Desarrollador Web</h2>
     </header>
   );
 };
