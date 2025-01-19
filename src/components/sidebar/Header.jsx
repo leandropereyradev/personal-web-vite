52992295d5c972ba1aa23bae84eca0ae042e85df
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-19 12:01:49 +0100

    Link to About in Header component

--- src/components/sidebar/Header.jsx
+++ src/components/sidebar/Header.jsx
@@ -1,11 +1,15 @@
 const Header = () => {
   return (
     <header className="flex flex-col gap-4 md:gap-10 lg:gap-6 xl:gap-8 fullHD:gap-12">
-      <h1 className="font-bold text-6xl leading-tight md:text-8xl lg:text-7xl xl:text-[5.5rem] fullHD:text-9xl">
-        Leandro <br />
-        <span className="block">Pereyra</span>
-      </h1>
-      <h2 className="text-blue-400 text-2xl md:text-4xl lg:text-3xl xl:text-[2.3rem] fullHD:text-[3.4rem]">Desarrollador Web</h2>
+      <a href="#about">
+        <h1 className="font-bold text-6xl leading-tight md:text-8xl lg:text-7xl fullHD:text-[5rem]">
+          Leandro <br />
+          <span className="block">Pereyra</span>
+        </h1>
+      </a>
+      <h2 className="text-blue-400 text-2xl md:text-4xl lg:text-3xl fullHD:text-[2.1rem]">
+        Desarrollador Web
+      </h2>
     </header>
   );
 };
