584c6fcb2778f3516485db72d2d743e607b092b2
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-20 14:57:58 +0100

    Fix BaseURL images Social component

--- src/components/sidebar/Social.jsx
+++ src/components/sidebar/Social.jsx
@@ -19,7 +19,7 @@ const Social = () => {
       {contactIcon.map((item, index) => (
         <a key={index} href={item.ref} target="_blank" rel="noreferrer">
           <img
-            src={`/assets/images/contact/${item.icon}.svg`}
+            src={`${import.meta.env.BASE_URL}assets/icons/${item.icon}.svg`}
             alt={item.icon}
             className="h-8 md:h-12 lg:h-9 xl:h-8"
           />
