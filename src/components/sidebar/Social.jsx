34aeef42206e9d1cfc84ee70e7a5a866982c9558
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-20 15:36:13 +0100

    Fix img src Social component

--- src/components/sidebar/Social.jsx
+++ src/components/sidebar/Social.jsx
@@ -1,13 +1,12 @@
 import icons from "../../data/social";
 
 const Social = () => {
-  
   return (
     <div className="flex gap-10 items-center md:gap-14 lg:gap-14 xl:gap-10">
       {icons.map((item, index) => (
         <a key={index} href={item.ref} target="_blank" rel="noreferrer">
           <img
-            src={`${import.meta.env.BASE_URL}assets/icons/${item.icon}.svg`}
+            src={`personal-web-vite/assets/icons/${item.icon}.svg`}
             alt={item.icon}
             className="h-8 md:h-12 lg:h-9 xl:h-8"
           />
