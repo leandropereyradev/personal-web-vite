dd97e7ae9615f0532fb0016bc01c8296f78daff7
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-21 09:36:15 +0100

    Fix spacing between components in MainContent

--- src/components/maincontent/MainContent.jsx
+++ src/components/maincontent/MainContent.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const MainContent = () => {
   return (
-    <section className="h-full flex flex-col gap-28 px-5 text-justify md:text-lg lg:overflow-y-auto xl:pl-36 xl:text-base fullHD:pl-72 fullHD:pr-10 fullHD:tracking-[0.2rem] fullHD:leading-relaxed">
+    <section className="h-full flex flex-col px-5 text-justify md:text-lg lg:overflow-y-auto xl:pl-36 xl:text-base fullHD:pl-72 fullHD:pr-10 fullHD:tracking-[0.2rem] fullHD:leading-relaxed">
       <About />
       <Projects />
       <Education />
--- src/components/maincontent/projects/Projects.jsx
+++ src/components/maincontent/projects/Projects.jsx
@@ -3,7 +3,7 @@ import projects from "../../../data/projects";
 
 const Projects = () => {
   return (
-    <section id="projects">
+    <section id="projects" className="pt-16">
       {/* Section Title */}
       <h1 className="text-blue-400 text-center text-2xl font-bold mb-6 md:text-3xl lg:pb-10 xl:text-3xl">
         Proyectos
