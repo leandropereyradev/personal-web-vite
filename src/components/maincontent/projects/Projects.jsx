c014755d98cbfda979271d79d444ae5716fe8c76
Leandro Pereyra <leandro-pereyra@hotmail.es>
2025-01-21 09:45:35 +0100

    Refactor Projects and add element in json

--- src/components/maincontent/projects/Projects.jsx
+++ src/components/maincontent/projects/Projects.jsx
@@ -1,22 +1,18 @@
 import Project from "./Project";
 import projects from "../../../data/projects";
 
-const Projects = () => {
-  return (
-    <section id="projects" className="pt-16">
-      {/* Section Title */}
-      <h1 className="text-blue-400 text-center text-2xl font-bold mb-6 md:text-3xl lg:pb-10 xl:text-3xl">
-        Proyectos
-      </h1>
+const Projects = () => (
+  <section id="projects" className="pt-16">
+    <h1 className="text-blue-400 text-center text-2xl font-bold mb-6 md:text-3xl lg:pb-10 xl:text-3xl">
+      Proyectos
+    </h1>
 
-      {/* Project List */}
-      <div className="flex flex-col gap-16">
-        {projects.map((project, index) => (
-          <Project key={index} {...project} />
-        ))}
-      </div>
-    </section>
-  );
-};
+    <div className="flex flex-col gap-16">
+      {projects.map((project, index) => (
+        <Project key={index} {...project} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Projects;
